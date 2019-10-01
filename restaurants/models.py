a643ad1c0d0e9387b0fe690512571e5d0c6109c5
Sarah Al Bader <s.r.albader@gmail.com>
2019-10-01 19:36:01 +0300

    completed task 4

--- restaurants/models.py
+++ restaurants/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length = 150)
     description = models.TextField()
-    opening_time = models.TextField()
-    closing_time =models.TextField()
+    opening_time = models.TimeField()
+    closing_time =models.TimeField()
 
     def __str__(self):
         return self.name
